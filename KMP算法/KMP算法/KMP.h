dcbbcca18b6b7629272ded3677adfc84e5ccd888
XieLong <1293336945@qq.com>
2017-04-30 21:53:22 +0800

    submit

---
+++ KMP算法/KMP算法/KMP.h
@@ -0,0 +1,82 @@
+﻿/***************************
+本来想设计成类
+但是想想没什么好设计的
+就普通函数了
+再封装成头文件
+顺便用上vector
+****************************/
+
+#pragma once
+#ifndef _KMP_H_
+#define _KMP_H_
+
+#ifndef False
+#define False -1
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+/* 设计Next */
+vector<int> getNext(const string sub)
+{
+	int len = sub.length();
+	vector<int> next(len + 4);
+	next.at(0) = 0;
+	next.at(1) = 0;								//初始化
+	int k = 0;
+	for (int i = 1; i < len; i++)
+	{
+		while (k > 0 && sub.at(k) != sub.at(i))					//这句while啃了一天。。。。
+			k = next.at(k);														//利用前面的信息进行匹配
+		if (sub.at(k) == sub.at(i))
+			k++;
+		next.at(i) = k;
+	}
+/*
+	for (k = 0; k < len; k++)
+		cout << next.at(k);
+	cout << endl;
+*/
+	return next;
+}
+
+/* 搜索字符串 */
+int kmpSearch(const string src, const string sub)
+{														//src源字符串，sub子字符串
+	int srclen = src.length();			//源串长度
+	int sublen = sub.length();			//子串长度
+	vector<int> next = getNext(sub);
+
+	int current = 0;				//当前位置
+	int subpos = 0;				//子串偏移位置
+	while (current < srclen)
+	{
+		while (subpos < sublen)
+		{
+			subpos++;
+			if (src.at(current + subpos - 1) == sub.at(subpos - 1))
+			{
+				if (subpos == sublen)
+					return current;								//找到匹配字符串，返回下标
+			}
+			else
+			{
+				current += subpos - next.at(subpos - 1);			//字符不匹配则后移
+				//cout << subpos;
+				subpos = 0;
+				break;
+			}
+		}
+	}
+	return False;					//查找失败
+}
+
+
+#endif // !False
+
+#endif // !_KMP_H_
+
---
+++ KMP算法/KMP算法/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include "KMP.h"
+
+using namespace std;
+
+vector<int> getNext(const string sub);
+int kmpSearch(const string src, const string sub);
+
+int main()
+{
+	string T = "ABCBABCABDABCD";
+	string S = "ABCAB";
+	cout << "Text：" << T << endl;
+	cout << "Sub：" << S << endl;
+	int result = kmpSearch(T, S);
+	if (result == False)
+		cout << "找不到该字符串" << endl;
+	else
+		cout << "该字符串第一次出现的位置是：" << result + 1 << endl;
+	system("pause");
+	return 0;
+}
